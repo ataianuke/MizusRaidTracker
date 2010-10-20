7e7abe78b3cf4f027180a92f1c7c4678f3a90473
Mizukichan <mizukichan@arcor.de>
2010-10-20 20:27:11 +0200

    Added esES boss yells for ICC Gunship Battle (Alliance), Blood Prince Council, Dreamwalker.

--- localization_esES.lua
+++ localization_esES.lua
@@ -41,27 +41,27 @@ MRT_L.Raidzones = {
 -----------------
 MRT_L.Bossyells = {
     -- Naxxramas
-    ["I grow tired of these games. Proceed, and I will banish your souls to oblivion!"] = "Four Horsemen",
+    ["I grow tired of these games. Proceed, and I will banish your souls to oblivion!"] = "Cofre de los Cuatro Jinetes",
 
     -- Ulduar
-    ["You rush headlong into the maw of madness!"] = "Iron Council",  -- Normalmode - Stormcaller Brundir last
-    ["What have you gained from my defeat? You are no less doomed, mortals!"] = "Iron Council",  -- Semi-Hardmode - Runemaster Molgeim last
-    ["Impossible..."] = function() return MRT_IsInstanceUlduar("Iron Council") end,  -- Hardmode - Steelbreaker last  // also yelled by Lich King -> instance check necessary
+    ["You rush headlong into the maw of madness!"] = "Asamblea de Hierro",  -- Normalmode - Stormcaller Brundir last
+    ["What have you gained from my defeat? You are no less doomed, mortals!"] = "Asamblea de Hierro",  -- Semi-Hardmode - Runemaster Molgeim last
+    ["Impossible..."] = function() return MRT_IsInstanceUlduar("Asamblea de Hierro") end,  -- Hardmode - Steelbreaker last  // also yelled by Lich King -> instance check necessary
     ["I... I am released from his grasp... at last."] = "Hodir",
     ["Stay your arms! I yield!"] = "Thorim",
     ["His hold on me dissipates. I can see clearly once more. Thank you, heroes."] = "Freya",
     ["It would appear that I've made a slight miscalculation. I allowed my mind to be corrupted by the fiend in the prison, overriding my primary directive. All systems seem to be functional now. Clear."] = "Mimiron",
-    ["I have seen worlds bathed in the Makers' flames, their denizens fading without as much as a whimper. Entire planetary systems born and razed in the time that it takes your mortal hearts to beat once. Yet all throughout, my own heart devoid of emotion... of empathy. I. Have. Felt. Nothing. A million-million lives wasted. Had they all held within them your tenacity? Had they all loved life as you do?"] = "Algalon the Observer",
+    ["I have seen worlds bathed in the Makers' flames, their denizens fading without as much as a whimper. Entire planetary systems born and razed in the time that it takes your mortal hearts to beat once. Yet all throughout, my own heart devoid of emotion... of empathy. I. Have. Felt. Nothing. A million-million lives wasted. Had they all held within them your tenacity? Had they all loved life as you do?"] = "Algalon el Observador",
 
     -- Trial of the Crusader
-    ["A shallow and tragic victory. We are weaker as a whole from the losses suffered today. Who but the Lich King could benefit from such foolishness? Great warriors have lost their lives. And for what? The true threat looms ahead - the Lich King awaits us all in death."] = "Faction Champions",
-    ["The Scourge cannot be stopped..."] = "Val'kyr Twins",
+    ["A shallow and tragic victory. We are weaker as a whole from the losses suffered today. Who but the Lich King could benefit from such foolishness? Great warriors have lost their lives. And for what? The true threat looms ahead - the Lich King awaits us all in death."] = "Campeones de la facción",
+    ["The Scourge cannot be stopped..."] = "las Gemelas Val'kyr",
         
     -- Icecrown Citadel
-    ["Don't say I didn't warn ya, scoundrels! Onward, brothers and sisters!"] = "Gunship Battle", -- Muradin
-    ["The Alliance falter. Onward to the Lich King!"] = "Gunship Battle", -- Saurfang
-    ["My queen, they... come."] = "Blood Prince Council", -- Prince Keleseth
-    ["I AM RENEWED! Ysera grant me the favor to lay these foul creatures to rest!"] = "Valithria Dreamwalker", -- Dreamwalker
+    ["¡No digáis que no lo avisé, sinvergüenzas! Adelante, hermanos."] = "Batalla aérea", -- Muradin
+    ["The Alliance falter. Onward to the Lich King!"] = "Batalla aérea", -- Saurfang
+    ["Mi reina, se... acercan."] = "Consejo de Príncipes de Sangre", -- Prince Keleseth
+    ["¡ESTOY RENOVADA! Ysera, haz que estas asquerosas criaturas descansen."] = "Valithria Caminasueños", -- Dreamwalker
 
     -- Ruby Sanctum
     ["Relish this victory, mortals, for it will be your last. This world will burn with the master's return!"] = "Halion", -- Halion
