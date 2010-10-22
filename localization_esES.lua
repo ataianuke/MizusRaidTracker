ffc48321c5c9b7b7ea44263c3473e51961f1184f
Mizukichan <mizukichan@arcor.de>
2010-10-22 11:13:12 +0200

    Deleted Iron Council (Steelbraker last) boss kill detection. There were too many problems with it.

--- localization_esES.lua
+++ localization_esES.lua
@@ -46,7 +46,7 @@ MRT_L.Bossyells = {
     -- Ulduar
     ["You rush headlong into the maw of madness!"] = "Asamblea de Hierro",  -- Normalmode - Stormcaller Brundir last
     ["What have you gained from my defeat? You are no less doomed, mortals!"] = "Asamblea de Hierro",  -- Semi-Hardmode - Runemaster Molgeim last
-    ["Impossible..."] = function() return MRT_IsInstanceUlduar("Asamblea de Hierro") end,  -- Hardmode - Steelbreaker last  // also yelled by Lich King -> instance check necessary
+    --["Impossible..."] = function() return MRT_IsInstanceUlduar("Asamblea de Hierro") end,  -- Hardmode - Steelbreaker last  // also yelled by Lich King -> instance check necessary
     ["I... I am released from his grasp... at last."] = "Hodir",
     ["Stay your arms! I yield!"] = "Thorim",
     ["His hold on me dissipates. I can see clearly once more. Thank you, heroes."] = "Freya",
