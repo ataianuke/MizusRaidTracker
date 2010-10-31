e450d180bc5c234e89283af35af93ca1354534a6
Mizukichan <mizukichan@arcor.de>
2010-10-31 14:10:00 +0100

    Updated koKR localization (big thanks to 7destiny)

--- localization_koKR.lua
+++ localization_koKR.lua
@@ -4,7 +4,7 @@
 -- *******************************************************
 --
 -- This localization is written by:
---  Mizukichan
+--  7destiny
 --
 -- Note: 
 --  MRT requires a correct localization of RaidZones and Bossyells for working
@@ -47,21 +47,21 @@ MRT_L.Bossyells = {
         ["You rush headlong into the maw of madness!"] = "무쇠 평의회",  -- Normalmode - Stormcaller Brundir last
         ["What have you gained from my defeat? You are no less doomed, mortals!"] = "무쇠 평의회",  -- Semi-Hardmode - Runemaster Molgeim last
         ["Impossible..."] = "무쇠 평의회",  -- Hardmode - Steelbreaker last
-        ["I... I am released from his grasp... at last."] = "호디르",  -- Hodir
-        ["Stay your arms! I yield!"] = "토림",  -- Thorim
-        ["His hold on me dissipates. I can see clearly once more. Thank you, heroes."] = "프레이야",  -- Freya
-        ["It would appear that I've made a slight miscalculation. I allowed my mind to be corrupted by the fiend in the prison, overriding my primary directive. All systems seem to be functional now. Clear."] = "미미론",  -- Mimiron
-        ["I have seen worlds bathed in the Makers' flames, their denizens fading without as much as a whimper. Entire planetary systems born and razed in the time that it takes your mortal hearts to beat once. Yet all throughout, my own heart devoid of emotion... of empathy. I. Have. Felt. Nothing. A million-million lives wasted. Had they all held within them your tenacity? Had they all loved life as you do?"] = "관찰자 알갈론",  -- Algalon
+        ["드디어... 드디어 그의 손아귀를... 벗어나는구나."] = "호디르",  -- Hodir
+        ["무기를 거둬라! 내가 졌다!"] = "토림",  -- Thorim
+        ["내게서 그의 지배력이 걷혔다. 다시 온전한 정신을 찾았도다. 영웅들이여, 고맙다."] = "프레이야",  -- Freya
+        ["^내가 계산을 좀 잘못한 것 같군"] = "미미론",  -- Mimiron
+        ["나는 창조주의 불길이 씻어내린 세상을 보았다."] = "관찰자 알갈론",  -- Algalon
     },
     
     ["Trial of the Crusader"] = {
-        ["A shallow and tragic victory. We are weaker as a whole from the losses suffered today. Who but the Lich King could benefit from such foolishness? Great warriors have lost their lives. And for what? The true threat looms ahead - the Lich King awaits us all in death."] = "진영 대표 용사",  -- Faction Champions
+        ["상처뿐인 승리로군."] = "진영 대표 용사",  -- Faction Champions
         ["The Scourge cannot be stopped..."] = "발키르 쌍둥이",  -- Val'kyr Twins
     },
         
     ["Icecrown Citadel"] = {
-        ["Don't say I didn't warn ya, scoundrels! Onward, brothers and sisters!"] = "얼음왕관 비행포격선 전투", -- Gunship Battle Muradin (A)
-        ["The Alliance falter. Onward to the Lich King!"] = "얼음왕관 비행포격선 전투", -- Gunship Battle Saurfang (H)
+        ["형제자매여, 전진"] = "얼음왕관 비행포격선 전투", -- Gunship Battle Muradin (A)
+        ["리치 왕을 향해 전진하라"] = "얼음왕관 비행포격선 전투", -- Gunship Battle Saurfang (H)
         ["My queen, they... come."] = "피의 공작 의회", -- Prince Keleseth
         ["I AM RENEWED! Ysera grant me the favor to lay these foul creatures to rest!"] = "발리스리아 드림워커", -- Dreamwalker
     },
