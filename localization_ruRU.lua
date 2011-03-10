17e913884687fe19fd2fdb968115b66fdb987fb5
Mizukichan <mizukichan@arcor.de>
2011-03-10 10:44:05 +0100

    Updated ruRU translation.

--- localization_ruRU.lua
+++ localization_ruRU.lua
@@ -4,7 +4,7 @@
 -- *******************************************************
 --
 -- This localization is written by:
---  rinaline, Mizukichan
+--  rinaline, YOti
 --
 -- Note: 
 --  MRT requires a correct localization of RaidZones and Bossyells for working
