e7f283f52982a677aab24f2976dec7d15ea3c826
Mizukichan <mizukichan@arcor.de>
2014-11-11 03:14:52 +0100

    Typo fixes

--- localization_enUS.lua
+++ localization_enUS.lua
@@ -73,23 +73,17 @@ MRT_L.Bossyells = {
     },
 }
 
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.529", namespace="Yells/Ulduar")@
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.543", namespace="Yells/Trials_of_the_Crusader")@
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.604", namespace="Yells/Icecrown_Citadel")@
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.609", namespace="Yells/Ruby_Sanctum")@
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.773", namespace="Yells/Throne_of_the_Four_Winds")@
 --@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.800", namespace="Yells/Firelands")@
-
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.886", namespace="Yells/Terrace_of_Endless_Spring")@
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.953", namespace="Yells/Siege_of_Orgrimmar")@
 --@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.994", namespace="Yells/Highmaul")@
 
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.604", namespace="Yells/Icecrown_Citadel")@
-
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.609", namespace="Yells/Ruby_Sanctum")@
-
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.953", namespace="Yells/Siege_of_Orgrimmar")@
-
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.886", namespace="Yells/Terrace_of_Endless_Spring")@
-
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.773", namespace="Yells/Throne_of_the_Four_Winds")@
-
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.543", namespace="Yells/Trials_of_the_Crusader")@
-
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="comment", table-name="mrt._L.yells.529", namespace="Yells/Ulduar")@
+--@localization(locale="enUS", format="lua_table", handle-unlocalized="english", namespace="Yells/Siege_of_Orgrimmar")@
 
 ---------------------------------
 --  Core frames local strings  --
