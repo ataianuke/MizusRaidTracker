37f97343da7fcd2d5a24ad17ec81fb3d04c558bf
Mizukichan <mizukichan@arcor.de>
2017-06-22 19:18:49 +0200

    Replaced dynamically created localization files with static ones (workaround until fixed)

--- localization_esMX.lua
+++ localization_esMX.lua
@@ -23,30 +23,18 @@ if GetLocale() ~= "esMX" then return end
 -----------------
 --  Bossyells  --
 -----------------
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[529]", namespace="Yells/Ulduar")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[543]", namespace="Yells/Trials_of_the_Crusader")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[604]", namespace="Yells/Icecrown_Citadel")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[609]", namespace="Yells/Ruby_Sanctum")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[773]", namespace="Yells/Throne_of_the_Four_Winds")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[800]", namespace="Yells/Firelands")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[886]", namespace="Yells/Terrace_of_Endless_Spring")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[953]", namespace="Yells/Siege_of_Orgrimmar")@
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="_L.yells[1026]", namespace="Yells/Hellfire_Citadel")@
 
 
 ---------------------------------
 --  Core frames local strings  --
 ---------------------------------
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="MRT_L.Core", namespace="MRT_L/Core")@
 
 
 -----------------------------------
 --  Option panels local strings  --
 -----------------------------------
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="MRT_L.Options", namespace="MRT_L/Options")@
 
 
 -------------------
 --  GUI strings  --
 -------------------
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="MRT_L.GUI", namespace="MRT_L/GUI")@
