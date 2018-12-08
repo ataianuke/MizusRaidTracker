974391e6791377b26eb1882317e697b0b26c5a18
Mizukichan <mizukichan@arcor.de>
2018-12-08 22:16:11 +0100

    Changed localization files for using the curse localization database again.

--- localization_itIT.lua
+++ localization_itIT.lua
@@ -1,6 +1,6 @@
 -- ********************************************************
 -- **           Mizus RaidTracker - itIT Local           **
--- **              <http://cosmocanyon.de>              **
+-- **              <http://cosmocanyon.de>               **
 -- ********************************************************
 --
 -- This localization is written by:
@@ -23,19 +23,31 @@ if GetLocale() ~= "itIT" then return end
 -----------------
 --  Bossyells  --
 -----------------
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[529]", namespace="Yells/Ulduar")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[543]", namespace="Yells/Trials_of_the_Crusader")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[604]", namespace="Yells/Icecrown_Citadel")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[609]", namespace="Yells/Ruby_Sanctum")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[773]", namespace="Yells/Throne_of_the_Four_Winds")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[800]", namespace="Yells/Firelands")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[886]", namespace="Yells/Terrace_of_Endless_Spring")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[953]", namespace="Yells/Siege_of_Orgrimmar")@
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="_L.yells[1026]", namespace="Yells/Hellfire_Citadel")@
 
 
 ---------------------------------
 --  Core frames local strings  --
 ---------------------------------
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="MRT_L.Core", namespace="MRT_L/Core")@
 
 
 -----------------------------------
 --  Option panels local strings  --
 -----------------------------------
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="MRT_L.Options", namespace="MRT_L/Options")@
 
 
 -------------------
 --  GUI strings  --
 -------------------
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="english", table-name="MRT_L.GUI", namespace="MRT_L/GUI")@
 
