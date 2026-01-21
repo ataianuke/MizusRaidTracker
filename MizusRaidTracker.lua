a4686ef8acb58779b9d919b2b6c98f5a83581808
Maximilian Ataian <83748743+ataianuke@users.noreply.github.com>
2026-01-21 17:46:30 +0100

    Bump version from v0.99 to v1.00

--- MizusRaidTracker.lua
+++ MizusRaidTracker.lua
@@ -45,7 +45,7 @@ local _O = MRT_Options
 MRT_ADDON_TITLE = C_AddOns.GetAddOnMetadata("MizusRaidTracker", "Title");
 MRT_ADDON_VERSION = C_AddOns.GetAddOnMetadata("MizusRaidTracker", "Version");
 --@debug@
-MRT_ADDON_VERSION = "v0.99"
+MRT_ADDON_VERSION = "v1.00"
 --@end-debug@
 MRT_NumOfCurrentRaid = nil;
 MRT_NumOfLastBoss = nil;
